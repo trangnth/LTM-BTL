8a56c95fa27188d02b49ae7df05b454d0d8c84a2
trangnth <nguyenhuyentrang1996@gmail.com>
2017-12-12 23:57:19 +0700

    add client.c

---
+++ Client/clinet.c
@@ -0,0 +1,29 @@
+/* 
+ * Client
+ * Cac chuc nang: 
+ 	1. Cho phep nguoi dung noi chuyen truc tiep
+	2. Cho phep nguoi dung noi chuyen theo nhom
+	3. Cho phep nguoi dung gui file cho nhau
+	4. Cho phep nguoi dung gui file theo nhom
+
+ * Cac thanh vien:
+	1. Nguyen Thi Huyen Trang
+	2. Le Quynh Trang
+	3. Hoang Van Thanh
+ * Note: Su dung mutithread de vua doc vua ghi mess
+*/
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+int main(){
+
+	return 0;
+}
